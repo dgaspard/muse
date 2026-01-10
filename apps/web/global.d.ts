bfe283a3a33384c13d73147bdc5f96e96152e01b
dgaspard <dgaspard@gmail.com>
2026-01-10 12:36:46 -0600

    test(web): add UI smoke test for upload page (vitest + testing-library)

---
+++ apps/web/global.d.ts
@@ -0,0 +1,9 @@
+declare module 'scheduler/tracing' {
+  export type Interaction = any
+  export function unstable_clear(..._args: any[]): any
+  export function unstable_getCurrent(..._args: any[]): any
+  export function unstable_subscribe(..._args: any[]): any
+  export function unstable_unsubscribe(..._args: any[]): any
+  export function unstable_trace(..._args: any[]): any
+  export const unstable_wrap: any
+}
